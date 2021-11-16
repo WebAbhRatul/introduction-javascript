c09dd49ed11d78a5083a5d52abc116739f8c503c
WebAbhRatul <web.abh.ratul@gmail.com>
2021-11-16 11:13:30 +0600

    Third commit

---
+++ comparisonOperator.js
@@ -0,0 +1,44 @@
+
+
+// Equal to Operator
+const a = 5, b = 7, c = 'hello';
+
+console.log(a == b);
+console.log('7' == b);
+console.log(a == c);
+
+
+// Not Equal to Operator
+const x = 4, y = 1, z = 'hello';
+console.log(x != y);
+console.log(y != z);
+
+
+// Strict Equal to Operator
+const a1 = 2, b1 = '2';
+console.log(a1 === b1);
+console.log(a1 !== b1);
+
+// Greater than Operator
+const a2 = 10, b2 = 20;
+console.log(a2 > b2);
+console.log(a2 < b2);
+
+// Less than or Equal to Operator
+const a3 = 2, b3 = 2, c3 = '2';
+console.log(a3 >= b3);
+console.log(a3 <= c3);
+
+
+// Logical AND Operator
+var abc = 4, abc1 = 5, abc2 = 6, abc3 = 7;
+console.log(abc < abc1 && abc2 < abc3);
+
+
+// Logical OR Operator
+var xyz = 10, xyz2 = 20, xyz3 = 30;
+console.log(xyz === 'hello' || xyz2 <= 30);
+
+
+
+
